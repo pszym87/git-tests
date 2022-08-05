9f6b16d7d51dc875c4d2c8d0c908ba5748a355d5
Piotr Szymanski <pszymanski@pc15.home>
2022-08-05 13:44:47 +0200

    Basic definitions of functions

---
+++ file1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "file1.h"
+
+int main(){
+
+	
+	return 0;
+}
+
+void fn_def(){
+	count = 0;
+	while(1) count++
+}
---
+++ file1.h
@@ -0,0 +1,4 @@
+void fn_def();
+int mul(int x,int y);
+int bi(int z);
+
